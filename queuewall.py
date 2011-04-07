ed372197de69036094f6979144d8e9a9c6affd0b
Marq Schneider <queueRAM@gmail.com>
2011-04-07 10:37:42 -0500

    Add option parsing from command line arguments.

--- queuewall.py
+++ queuewall.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import getpass
+import optparse
 import os
 import platform
 import select
@@ -9,29 +10,29 @@ import sys
 import threading
 import time
 
-################################ CONFIGURATION #################################
+##################### DEFAULT CONFIGURATION #################################
 # This directory should contain images that are named the hour (0-23) of the day
 # e.g. midnight = 00.jpg, 7AM = 07.jpg, noon = 12.jpg, 5PM = 17.jpg
-WALLPAPER_DIR = os.path.expanduser('~') + "/docs/images/wallpapers".replace("/", os.sep)
-IMAGE_EXTENSION = ".jpg"
+DEFAULT_WALLPAPER_DIR = os.path.expanduser('~') + "/docs/images/wallpapers".replace("/", os.sep)
 
-# Choose one of: autodetect, gnome, xfce4, lxde, windows, other
-DESKTOP_ENV = "autodetect"
+DEFAULT_IMAGE_EXTENSION = "jpg"
 
-ENABLE_LOGGING = False
-############################### END CONFIGURATION ##############################
+########################### END DEFAULT CONFIGURATION #######################
 
 
 #################################### Classes ###################################
 class DesktopEnvironment(object):
    def __init__(self, name):
       self.name = name
+   def setCommand(self, command):
+      self.command = command
    def setWallpaper(self, path):
       print("Wallpaper: %s" % path)
       
 # Wallpaper setter for Linux (and other POSIX) family of operating systems
 class LinuxDE(DesktopEnvironment):
    def __init__(self, name):
+      self.command = None
       ################################ Commands ################################
       # The commands to run for each desktop environment.
       # The first occurrence of %s will be replaced with the path to the image
@@ -46,14 +47,18 @@ class LinuxDE(DesktopEnvironment):
          self.name = name
 
    def setWallpaper(self, file_path):
-      if self.name in self.COMMANDS:
-         # lookup the command and replace "%s" with the path to the file
-         command = self.COMMANDS[self.name] % file_path
-         os.system(command)
-         log("Setting wallpaper: " + file_path)
+      # lookup the command and replace "%s" with the path to the file
+      if (self.command != None):
+         command = self.command % file_path
       else:
-         log(DESKTOP_ENV + " not in list of known commands")
-         exit(1)
+         # no custom command set, lookup command in dictionary
+         if self.name in self.COMMANDS:
+            command = self.COMMANDS[self.name] % file_path
+         else:
+            log(self.name + " not in list of known commands")
+            sys.exit(1)
+      os.system(command)
+      log("Setting wallpaper: " + file_path)
 
    # attempt to detect the desktop environment that the user is running
    def detectEnvironment(self):
@@ -109,33 +114,37 @@ class WindowsDE(DesktopEnvironment):
 ################################### Functions ##################################
 # log a message
 # TODO: log to a file
+def logInit(enable):
+   global enableLogging
+   enableLogging = enable
+
 def log(inStr):
-   if ENABLE_LOGGING:
+   if enableLogging:
       print(time.ctime() + ": " + inStr)
 
 # get the current Desktop Environment
-def currentDE():
-   if DESKTOP_ENV == "autodetect":
+def currentDE(name):
+   if name == "autodetect":
       # posix
       if platform.system().lower().startswith("linux"):
-         de = LinuxDE(DESKTOP_ENV)
+         de = LinuxDE(name)
       # assume windows
       else:
          de = WindowsDE("windows")
       log("autodetected \"" + de.name + "\"")
    else:
-      if DESKTOP_ENV == "windows":
+      if name == "windows":
          de = WindowsDE("windows")
       else:
-         de = LinuxDE(DESKTOP_ENV)
+         de = LinuxDE(name)
 
    return de
 
-def changeWallpaper(de, ev):
+def changeWallpaper(options, de, ev):
    cur_time = time.localtime()
-   imagename = "%02d" % cur_time.tm_hour + IMAGE_EXTENSION
+   imagename = "%02d" % cur_time.tm_hour + "." + options.extension
    # if the hour has rolled over, or first time through
-   wallpaper = WALLPAPER_DIR + os.sep + imagename
+   wallpaper = options.directory + os.sep + imagename
    if os.path.exists(wallpaper):
       de.setWallpaper(wallpaper)
    else:
@@ -155,11 +164,12 @@ class CommandLineThread(threading.Thread):
    def run(self):
       command = ""
       sys.stdout.write("queueWall> ")
-      timeout = 5
+      sys.stdout.flush()
+      timeout = 1
       while(command != "exit"):
          rlist, _, _ = select.select([sys.stdin], [], [], timeout)
          if rlist:
-            command = sys.stdin.readline()
+            command = sys.stdin.readline().strip()
             if(command == "help"):
                print("Commands: exit, help, restart")
             elif(command == "restart") or (command == "exit"):
@@ -168,24 +178,46 @@ class CommandLineThread(threading.Thread):
             else:
                print("Unknown command: %s" % command)
             sys.stdout.write("queueWall> ")
+            sys.stdout.flush()
          elif self.in_ev.isSet():
-            return
+            command = "exit"
+      print("")
+      log("CommandLine returning...")
 
 #################################### main ######################################
 if __name__ == "__main__":
-   run_command_line = False
+   parser = optparse.OptionParser(usage="%prog [options]", \
+                                  version="%prog 0.00")
+   parser.add_option("-c", "--command", help="custom command to change wallpaper [example: \"feh --bg-scale %s\"]")
+   parser.add_option("-d", "--directory", help="wallpapers directory [default: %default]", \
+                     default=DEFAULT_WALLPAPER_DIR)
+   parser.add_option("-e", "--extension", help="image extension [default: %default]", \
+                     default=DEFAULT_IMAGE_EXTENSION)
+   parser.add_option("-l", "--log", action="store_true", \
+                     help="enable logging", default=False)
+   parser.add_option("-s", "--system", help="currently running system: autodetect, gnome, xfce4, lxde, windows [default: %default]", \
+                     default="autodetect")
+   parser.add_option("-t", "--terminal", action="store_true", \
+                     help="allow commands to be entered from terminal", default=False)
 
-   de = currentDE()
+   (options, args) = parser.parse_args()
+
+   logInit(options.log)
+
+   de = currentDE(options.system)
+   if options.command != None:
+      log("Using command: %s" % options.command)
+      de.setCommand(options.command)
 
    # event flag to wait until thread completes
    command_ev = threading.Event()
    de_ev = threading.Event()
 
    # call first time through to ensure background for current time is set
-   changeWallpaper(de, command_ev)
+   changeWallpaper(options, de, de_ev)
 
    # spawn command line thread
-   if run_command_line:
+   if options.terminal:
       fifo = []
       command_thread = CommandLineThread(command_ev, de_ev, fifo).start()
 
@@ -198,17 +230,17 @@ if __name__ == "__main__":
          cur_time = time.localtime()
          # TODO: this is where the scheduler would figure out the delay
          delay_time = 60 * (60 - cur_time.tm_min) - cur_time.tm_sec
-         t = threading.Timer(delay_time, changeWallpaper, [de, de_ev])
+         t = threading.Timer(delay_time, changeWallpaper, [options, de, de_ev])
          log("main: delay time: %d (s)" % delay_time)
          t.start()
          # wait until thread runs and signals its completion
          log("main: waiting")
          de_ev.wait()
          log("main: done waiting")
-         if run_command_line and (len(fifo)) > 0:
+         if options.terminal and (len(fifo)) > 0:
             command = fifo.pop()
             if(command == "exit"):
-               log("main: exitting...")
+               log("main: exiting...")
                t.cancel()
                running = False
             elif(command == "restart"):
@@ -218,7 +250,7 @@ if __name__ == "__main__":
                log("main: command: %s" % command)
    except KeyboardInterrupt:
       command_ev.set()
-      log("Handling KeyboardInterrupt.  Exiting...")
+      print("Handling KeyboardInterrupt.  Exiting...")
       # Cancel the pending thread.  This can be called even if it has already started.
       t.cancel()
 
